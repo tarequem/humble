6597692eb9f6924f0dddc7337f049b2b97297973
Vadad Rzali <vedad.rzali@gmail.com>
2022-08-15 00:45:40 -0400

    remove unused import

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,7 +3,6 @@ const sequelize = require('../../config/connection');
 const { Post, User, Comment, Vote } = require('../../models');
 const withAuth = require('../../utils/auth');
 const postController = require('../postController');
-const multipart = require('parse-multipart-data');
 
 // get all users
 router.get('/', (req, res) => {
