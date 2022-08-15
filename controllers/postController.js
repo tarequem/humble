72fc35d98361ef8ed23c94339bebcbd9c482229c
Vadad Rzali <vedad.rzali@gmail.com>
2022-08-15 11:36:54 -0400

    Clean up code

--- controllers/postController.js
+++ controllers/postController.js
@@ -15,29 +15,6 @@ const createPost = async (req, res) => {
   }
 };
 
-// var storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'Images');
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: '1000000' },
-//   fileFilter: (req, file, cb) => {
-//       const fileTypes = /jpeg|jpg|png/;
-//       const mimeType = fileTypes.test(file.mimetype);
-//       const extname = fileTypes.test(path.extname(file.originalname));
-
-//       if(mimeType && extname) {
-//           return cb(null, true);
-//       }
-//       cb('Give proper files formate to upload');
-//   }
-// }).single('image');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
